151859f56fe321f51240ed67d345f55e0e299b56
Matias Ortiz <matias.ortiz.canar@udla.edu.ec>
2026-05-22 15:03:37 -0500

    Commit 2: Aplicacion de sizeof y promedio de notas

--- Ejercicios.c
+++ Ejercicios.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 int main(){
 float notas[]= {6.5 , 7.1 , 6.5 , 6.1};
+float suma = 0;
+int n = sizeof (notas) / sizeof (notas[0]);
 for (int i = 0; i < 5; i++)
 {
     printf("notas %d: %.1f\n", i + 1, notas[i]);
+    suma = suma + notas[i];
 }
+printf("EL promedio es %.2f", suma/n);
 }
