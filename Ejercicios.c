36ccf66b572a5a14d37df549125a37543e5e07ff
Matias Ortiz <matias.ortiz.canar@udla.edu.ec>
2026-05-22 15:16:54 -0500

    Commit 3: Paso de parametros por referencia

--- Ejercicios.c
+++ Ejercicios.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
-int main(){
+float calcular_promedio(float*a); //a = suma total
 float notas[]= {6.5 , 7.1 , 6.5 , 6.1};
-float suma = 0;
 int n = sizeof (notas) / sizeof (notas[0]);
-for (int i = 0; i < 5; i++)
+int main(){
+//printf("EL promedio es %.2f", suma/n);
+printf("El promedio puntero es: %.2f", calcular_promedio(notas));
+return 0;
+}
+float calcular_promedio(float*a){
+    float suma = 0;
+    for (int i = 0; i < 5; i++)
 {
-    printf("notas %d: %.1f\n", i + 1, notas[i]);
+    printf("nota %d: %.1f\n", i + 1, notas[i]);
     suma = suma + notas[i];
 }
-printf("EL promedio es %.2f", suma/n);
+    return *a / 5;
 }
